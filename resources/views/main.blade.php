4bf2d6c7bbb9cbce78e1853e6c6be8f79a02aaad
unknown <azmiagil05@gmail.com>
2025-10-29 08:12:57 +0700

    fitur chatbot

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -30,7 +30,19 @@
         }
     }
 </script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/690014706b37cd19503f2abd/1j8k4vmhh';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <div id="splash-screen">
         <img src="{{ asset('assets/images/logo.png') }}" alt="Splash Logo" class="splash-logo" />
